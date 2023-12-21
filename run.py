0ad7de0bc5b543950dfe3fefdf740274cedf5751
jmpaz <30947643+jmpaz@users.noreply.github.com>
2023-12-20 20:02:30 -0500

    Add LRC support

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import argparse
-from transcribe.transcribe import call_whisperx
+import os
+import shutil
+from transcribe.transcribe import call_whisperx, convert_to_lrc
 
 
 def parse_arguments():
@@ -18,17 +20,34 @@ def parse_arguments():
     parser.add_argument(
         "-f",
         "--format",
-        choices=["srt", "json"],
-        default=None,
-        help="Output format: 'srt', 'json'. Default is 'srt'.",
+        choices=["srt", "json", "lrc"],
+        default="srt",
+        help="Output format: 'srt', 'json', 'lrc'.",
     )
     return parser.parse_args()
 
 
+def create_temp_directory():
+    temp_dir = "tmp"
+    os.makedirs(temp_dir, exist_ok=True)
+    return temp_dir
+
+
 if __name__ == "__main__":
     args = parse_arguments()
+
+    if not os.path.exists(args.input_path):
+        raise FileNotFoundError(f"Input path {args.input_path} does not exist.")
+
+    temp_dir = create_temp_directory() if args.format == "lrc" else args.output
+
     call_whisperx(
         args.input_path,
         args.output,
         output_format=args.format if args.format else "srt",
     )
+
+    # Convert to LRC format if specified, then remove the temporary directory
+    if args.format == "lrc":
+        convert_to_lrc(temp_dir, args.output)
+        shutil.rmtree(temp_dir)
--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+import re
 
 
 def call_whisperx(
@@ -36,3 +38,29 @@ def call_whisperx(
         subprocess.run(cmd, check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while running whisperx: {e}")
+
+
+def srt_timestamp_to_lrc(timestamp):
+    # SRT format: '00:01:02,500' -> LRC format: '[01:02.00]'
+    match = re.match(r"(\d+):(\d+):(\d+),\d+", timestamp)
+    if match:
+        return f"[{match.group(2)}:{match.group(3)}.0]"
+    return "[00:00.0]"
+
+
+def convert_to_lrc(temp_dir, output_dir):
+    for srt_file in os.listdir(temp_dir):
+        if srt_file.endswith(".srt"):
+            srt_path = os.path.join(temp_dir, srt_file)
+            lrc_path = os.path.join(output_dir, os.path.splitext(srt_file)[0] + ".lrc")
+
+            with open(srt_path, "r") as srt, open(lrc_path, "w") as lrc:
+                lines = srt.readlines()
+                for i, line in enumerate(lines):
+                    if "-->" in line:
+                        start_time, _ = line.split(" --> ")
+                        lrc_timestamp = srt_timestamp_to_lrc(start_time.strip())
+                        # Check for text in the next line
+                        if i + 1 < len(lines):
+                            text_content = lines[i + 1].strip()
+                            lrc.write(f"{lrc_timestamp}  {text_content}\n")
