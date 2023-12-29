186edb12978c6f849f01fbbae1dd00ec5c1054b5
jmpaz <30947643+jmpaz@users.noreply.github.com>
2023-12-28 22:00:46 -0500

    Refactor run.py for subcommands
    - Prepare for upcoming features by implementing `transcribe` as initial subcommand
    - Modify `--relocate_files` to accept true/false

--- run.py
+++ run.py
@@ -4,77 +4,31 @@ import shutil
 from core.transcribe import call_whisperx, convert_to_lrc
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Transcribe audio with WhisperX.")
-    parser.add_argument(
-        "input_path", help="Path to the audio file or directory to transcribe."
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        default="data/processed",
-        help="Output directory for the files. Default is 'data/processed'.",
-    )
-    parser.add_argument(
-        "--relocate_files",
-        action="store_true",
-        default=True,
-        help="Move transcribed audio files to the output directory upon completion.",
-    )
-    parser.add_argument(
-        "-f",
-        "--format",
-        choices=["srt", "json", "lrc"],
-        default="srt",
-        help="Output format: 'srt', 'json', 'lrc'.",
-    )
-    parser.add_argument(
-        "--speaker_count",
-        type=int,
-        help="Set minimum and maximum number of speakers.",
-    )
-    parser.add_argument(
-        "--device_index",
-        type=int,
-        help="Index of the device to use.",
-    )
-    parser.add_argument(
-        "--prompt",
-        type=str,
-        help="Initial prompt to use.",
-    )
-    return parser.parse_args()
+def transcribe(args):
+    def process_audio_file(file_path, temp_dir, args):
+        call_whisperx(
+            file_path,
+            temp_dir,
+            output_format="srt",
+            speaker_count=args.speaker_count,
+            device_index=args.device_index,
+            initial_prompt=args.prompt,
+        )
+        if args.format == "lrc":
+            convert_to_lrc(temp_dir, args.output)
+        if args.relocate_files == "true":
+            shutil.move(file_path, args.output)
 
+    def process_directory(directory_path, args):
+        temp_dir = "tmp" if args.format == "lrc" else args.output
+        os.makedirs(temp_dir, exist_ok=True)
 
-def process_audio_file(file_path, temp_dir, args):
-    call_whisperx(
-        file_path,
-        temp_dir,
-        output_format="srt",
-        speaker_count=args.speaker_count,
-        device_index=args.device_index,
-        initial_prompt=args.prompt,
-    )
-    if args.format == "lrc":
-        convert_to_lrc(temp_dir, args.output)
-    if args.relocate_files:
-        shutil.move(file_path, args.output)
+        for filename in os.listdir(directory_path):
+            file_path = os.path.join(directory_path, filename)
+            process_audio_file(file_path, temp_dir, args)
 
-
-def process_directory(directory_path, args):
-    temp_dir = "tmp" if args.format == "lrc" else args.output
-    os.makedirs(temp_dir, exist_ok=True)
-
-    for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)
-        process_audio_file(file_path, temp_dir, args)
-
-    if args.format == "lrc":
-        shutil.rmtree(temp_dir)
-
-
-if __name__ == "__main__":
-    args = parse_arguments()
+        if args.format == "lrc":
+            shutil.rmtree(temp_dir)
 
     if not os.path.exists(args.input_path):
         raise FileNotFoundError(f"Input path {args.input_path} does not exist.")
@@ -92,3 +46,57 @@ if __name__ == "__main__":
         process_audio_file(args.input_path, temp_dir, args)
         if args.format == "lrc":
             shutil.rmtree(temp_dir)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="CLI for audio transcription and processing."
+    )
+    subparsers = parser.add_subparsers(help="sub-command help", dest="command")
+
+    # Subparser for the 'transcribe' command
+    parser_transcribe = subparsers.add_parser(
+        "transcribe", help="Transcribe audio files."
+    )
+    parser_transcribe.add_argument(
+        "input_path", help="Path to the audio file or directory to transcribe."
+    )
+    parser_transcribe.add_argument(
+        "-o",
+        "--output",
+        default="data/processed",
+        help="Output directory for the files. Default is 'data/processed'.",
+    )
+    parser_transcribe.add_argument(
+        "--relocate_files",
+        choices=["true", "false"],
+        default="true",
+        help="Whether to move transcribed audio files to the output directory upon completion. Accepts 'true' or 'false'; default is 'true'.",
+    )
+    parser_transcribe.add_argument(
+        "-f",
+        "--format",
+        choices=["srt", "json", "lrc"],
+        default="srt",
+        help="Output format: 'srt', 'json', 'lrc'.",
+    )
+    parser_transcribe.add_argument(
+        "--speaker_count", type=int, help="Set minimum and maximum number of speakers."
+    )
+    parser_transcribe.add_argument(
+        "--device_index", type=int, help="Index of the device to use."
+    )
+    parser_transcribe.add_argument("--prompt", type=str, help="Initial prompt to use.")
+    parser_transcribe.set_defaults(func=transcribe)
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    if args.command is None:
+        parser.print_help()
+    else:
+        args.func(args)
+
+
+if __name__ == "__main__":
+    main()
