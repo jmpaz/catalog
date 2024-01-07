83afcfa44dd178199d0982ece72aeee7ed5ef67a
jmpaz <30947643+jmpaz@users.noreply.github.com>
2024-01-06 20:19:08 -0500

    Extract audio processing into class `AudioHandler`

--- app.py
+++ app.py
@@ -26,6 +26,101 @@ def format_duration(duration):
     return " ".join(duration_parts)
 
 
+class AudioHandler:
+    def __init__(self, args, logger, console):
+        self.args = args
+        self.logger = logger
+        self.console = console
+        self.transcriber = Transcriber(device_index=args.device_index)
+        self.temp_dir = ".tmp" if args.format == "lrc" else args.output
+        os.makedirs(self.temp_dir, exist_ok=True)
+
+    def transcribe(self):
+        print(f"Transcribe command received for {self.args.input_path}.\n")
+        self.logger.start_session(self.args)
+
+        if not os.path.exists(self.args.input_path):
+            raise FileNotFoundError(
+                f"Input path {self.args.input_path} does not exist."
+            )
+        elif os.path.isdir(self.args.input_path) and not os.listdir(
+            self.args.input_path
+        ):
+            raise FileNotFoundError(f"Input directory {self.args.input_path} is empty.")
+
+        if not os.path.exists(self.args.output):
+            os.makedirs(self.args.output)
+
+        if os.path.isdir(self.args.input_path):
+            self.process_directory(self.args.input_path)
+        else:
+            self.process_audio_file(self.args.input_path)
+
+        self.finalize_lrc()
+        self.logger.end_session()
+
+    def process_audio_file(self, file_path):
+        start_time = datetime.now()
+        if self.args.verbose:
+            print(f"Transcribing {file_path}...")
+
+        self.transcriber.call_whisperx(
+            file_path,
+            self.temp_dir,
+            output_format="srt",
+            speaker_count=self.args.speaker_count,
+            initial_prompt=self.args.prompt,
+        )
+
+        if self.args.relocate_files == "true":
+            shutil.move(file_path, self.args.output)
+
+        end_time = datetime.now()
+        self.logger.log_file_process(file_path, start_time, end_time)
+
+        if self.args.verbose:
+            print(
+                f"Finished processing {file_path} in {format_duration(end_time - start_time)}\n"
+            )
+
+    def process_directory(self, directory_path):
+        self.console.print(f"Processing directory: {directory_path}", style="bold")
+        start_time = datetime.now()
+        file_list = sorted(os.listdir(directory_path))
+        total_files = len(file_list)
+
+        with Progress(
+            "[progress.description]{task.description}",
+            BarColumn(bar_width=40),
+            TextColumn("[bold]{task.fields[file_count]}", justify="right"),
+            SpinnerColumn(spinner_name="dots", style="bold green"),
+            console=self.console,
+        ) as progress:
+            task_id = progress.add_task(
+                "", total=total_files, file_count=f"0/{total_files}"
+            )
+            for index, filename in enumerate(file_list, start=1):
+                file_path = os.path.join(directory_path, filename)
+                self.process_audio_file(file_path)
+                progress.update(task_id, advance=1, file_count=f"{index}/{total_files}")
+
+        if self.args.format == "lrc":
+            to_lrc(self.temp_dir, self.args.output)
+            shutil.rmtree(self.temp_dir)
+
+        end_time = datetime.now()
+        duration = format_duration(end_time - start_time)
+        self.console.print(
+            f"Finished processing {total_files} files in {duration}.",
+            style="green bold",
+        )
+
+    def finalize_lrc(self):
+        if os.path.exists(self.temp_dir) and self.args.format == "lrc":
+            to_lrc(self.temp_dir, self.args.output)
+            shutil.rmtree(self.temp_dir)
+
+
 class ArgParser:
     def __init__(self):
         self.console = Console()
@@ -76,102 +171,14 @@ class ArgParser:
         parser_transcribe.add_argument(
             "--prompt", type=str, help="Initial prompt to use."
         )
-        parser_transcribe.set_defaults(func=self.transcribe)
+        parser_transcribe.set_defaults(func=self.handle_transcription)
+
+    def handle_transcription(self, args, logger):
+        audio_handler = AudioHandler(args, logger, self.console)
+        audio_handler.transcribe()
 
     def parse_args(self):
         return self.parser.parse_args()
-
-    def transcribe(self, args, logger):
-        print(
-            f"Transcribe command received for {args.input_path}.\n",
-        )
-        logger.start_session(args)
-
-        if not os.path.exists(args.input_path):
-            raise FileNotFoundError(f"Input path {args.input_path} does not exist.")
-        elif os.path.isdir(args.input_path) and not os.listdir(args.input_path):
-            raise FileNotFoundError(f"Input directory {args.input_path} is empty.")
-
-        if not os.path.exists(args.output):
-            os.makedirs(args.output)
-
-        transcriber = Transcriber(device_index=args.device_index)
-
-        if os.path.isdir(args.input_path):
-            self.process_directory(args.input_path, args, logger, transcriber)
-        else:
-            # If the input is a single file, create a temporary directory to store the output
-            temp_dir = ".tmp" if args.format == "lrc" else args.output
-            os.makedirs(temp_dir, exist_ok=True)
-            self.process_audio_file(
-                args.input_path, temp_dir, args, logger, transcriber
-            )
-            if args.format == "lrc":
-                shutil.rmtree(temp_dir)
-
-        logger.end_session()
-
-    def process_audio_file(self, file_path, temp_dir, args, logger, transcriber):
-        start_time = datetime.now()
-
-        if args.verbose:
-            print(f"Transcribing {file_path}...")
-
-        transcriber.call_whisperx(
-            file_path,
-            temp_dir,
-            output_format="srt",
-            speaker_count=args.speaker_count,
-            initial_prompt=args.prompt,
-        )
-
-        if args.format == "lrc":
-            to_lrc(temp_dir, args.output)
-        if args.relocate_files == "true":
-            shutil.move(file_path, args.output)
-
-        end_time = datetime.now()
-        logger.log_file_process(file_path, start_time, end_time)
-
-        if args.verbose:
-            print(
-                f"Finished processing {file_path} in {format_duration(end_time - start_time)}\n"
-            )
-
-    def process_directory(self, directory_path, args, logger, transcriber):
-        self.console.print(f"Processing directory: {directory_path}", style="bold")
-        start_time = datetime.now()
-        file_list = sorted(os.listdir(directory_path))
-        total_files = len(file_list)
-
-        temp_dir = ".tmp" if args.format == "lrc" else args.output
-        os.makedirs(temp_dir, exist_ok=True)
-
-        with Progress(
-            "[progress.description]{task.description}",
-            BarColumn(bar_width=40),
-            TextColumn("[bold]{task.fields[file_count]}", justify="right"),
-            SpinnerColumn(spinner_name="dots", style="bold green"),
-            console=self.console,
-        ) as progress:
-            task_id = progress.add_task(
-                "", total=total_files, file_count=f"0/{total_files}"
-            )
-            for index, filename in enumerate(file_list, start=1):
-                file_path = os.path.join(directory_path, filename)
-                self.process_audio_file(file_path, temp_dir, args, logger, transcriber)
-                progress.update(task_id, advance=1, file_count=f"{index}/{total_files}")
-
-        if args.format == "lrc":
-            to_lrc(temp_dir, args.output)
-            shutil.rmtree(temp_dir)
-
-        end_time = datetime.now()
-        duration = format_duration(end_time - start_time)
-        self.console.print(
-            f"Finished processing {total_files} files in {duration}.",
-            style="green bold",
-        )
 
 
 def main():
@@ -185,8 +192,7 @@ def main():
         arg_parser.parser.print_help()
     else:
         logger = Logger("logs/log.json")
-        args.func(args, logger)
-        logger.save_log()
+        arg_parser.handle_transcription(args, logger)
 
 
 if __name__ == "__main__":
